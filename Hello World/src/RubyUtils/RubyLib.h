378015d8e098021bfb8d982c6c1bdb4ce26c6ccc
Thomas Thomassen <thomas@thomthom.net>
2016-06-14 10:43:54 -0600

    Making changes to allow VS2015 to build.

--- Hello World/src/RubyUtils/RubyLib.h
+++ Hello World/src/RubyUtils/RubyLib.h
@@ -1,6 +1,11 @@
 #ifndef SU_UTILS_RUBYLIB_H_
 #define SU_UTILS_RUBYLIB_H_
 
+// Needed for VS2015.
+#if _MSC_VER >= 1900
+#define HAVE_STRUCT_TIMESPEC 1
+#endif
+
 #include <ruby.h>
 #ifdef HAVE_RUBY_ENCODING_H
 #include <ruby/encoding.h>
