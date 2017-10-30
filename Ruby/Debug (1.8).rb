21a20768caaf1425399070575acf297cb3eee8ff
Thomas Thomassen <thomas@thomthom.net>
2017-10-30 14:20:45 +0100

    Simplify how SketchUp is launched from Visual Studio.

--- Ruby/Debug (1.8).rb
+++
@@ -1,2 +0,0 @@
-RELEASE = File.basename(__FILE__, '.*').freeze
-require File.join( File.dirname(__FILE__), 'setup_environment.rb' )
--- Ruby/launch_sketchup.rb
+++ Ruby/launch_sketchup.rb
@@ -1,19 +1,29 @@
 # This file should be required by a file that has defined `RELEASE` to be the
 # name of the build folder where the binaries are located.
 
+configuration, platform = ARGV[0].split(':')
+
 if SKETCHUP_CONSOLE.respond_to?(:show)
   SKETCHUP_CONSOLE.show
 else
   Sketchup.send_action("showRubyPanel:")
 end
 
-prostatus = Sketchup.is_pro? ? "Pro" : ""
-puts "SketchUp #{prostatus} #{Sketchup.version}"
-puts "Loading '#{RELEASE}' build..."
+pro_status = Sketchup.is_pro? ? "Pro" : ""
+puts "SketchUp #{pro_status} #{Sketchup.version}"
+puts "Loading '#{configuration}' (#{platform}) build..."
 
 ruby_path = File.dirname(__FILE__)
 project_path = File.expand_path( File.join(ruby_path, '..') )
-binary_path = File.join(project_path, RELEASE)
+if platform == 'x64'
+  binary_path = File.join(project_path, configuration, platform)
+else
+  binary_path = File.join(project_path, configuration)
+end
+
+puts ruby_path
+puts project_path
+puts binary_path
 
 pattern = File.join(binary_path, "*.{so,bundle}")
 Dir.glob(pattern).each { |library|
