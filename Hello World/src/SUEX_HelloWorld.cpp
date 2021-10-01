192d291ce6148585ac07704650bce46801e0b7de
Thomas Thomassen <thomthom@sketchup.com>
2021-10-01 14:44:40 +0200

    Add per-platform config for Ruby's include headers.

--- Hello World/src/SUEX_HelloWorld.cpp
+++ Hello World/src/SUEX_HelloWorld.cpp
@@ -6,6 +6,10 @@ VALUE hello_world() {
   return GetRubyInterface("Hello World!");
 }
 
+VALUE ruby_platform() {
+  return GetRubyInterface(RUBY_PLATFORM);
+}
+
 // Load this module from Ruby using:
 //   require 'SUEX_HelloWorld'
 extern "C"
@@ -13,6 +17,6 @@ void Init_SUEX_HelloWorld()
 {
   VALUE mSUEX_HelloWorld = rb_define_module("SUEX_HelloWorld");
   rb_define_const(mSUEX_HelloWorld, "CEXT_VERSION", GetRubyInterface("1.0.0"));
-  rb_define_module_function(mSUEX_HelloWorld, "hello_world",
-    VALUEFUNC(hello_world), 0); 
+  rb_define_module_function(mSUEX_HelloWorld, "hello_world", VALUEFUNC(hello_world), 0);
+  rb_define_module_function(mSUEX_HelloWorld, "ruby_platform", VALUEFUNC(ruby_platform), 0);
 }
